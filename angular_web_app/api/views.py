46d4e12ee0c64f52634f1c831e19c974c951f0a8
Shamsher Singh Chauhan <87360045+Shamsherocode@users.noreply.github.com>
2022-01-27 18:57:21 +0530

    Create login, register api and connect with angular.
    Create login, register api and connect with angular.

---
+++ angular_web_app/api/views.py
@@ -0,0 +1,47 @@
+import json
+from django.contrib.auth import login
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from rest_framework import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+from .serializers import UserSerializer, RegisterSerializer
+from knox.models import AuthToken
+from rest_framework.response import Response
+from rest_framework import generics, permissions
+from django.core import serializers
+
+
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = {
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        }
+        return Response(data)
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        resp =  super(LoginAPI, self).post(request, format=None)
+        resp.data["username"] = user.username
+        resp.data["email"] = user.email
+        return resp
+
+
+def error_404_view(request, exception):
+    data = {"name": "ThePythonDjango.com"}
+    return render(request,'myapp/error_404.html', data)
